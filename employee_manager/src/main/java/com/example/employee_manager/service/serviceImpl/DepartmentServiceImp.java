dd632b237344283483f053661952ca66fcf16f93
dinhxuanloc97 <dinhxuanloc741997@gmail.com>
2023-05-30 10:16:46 +0700

    feat:createDate-department

--- employee_manager/src/main/java/com/example/employee_manager/service/serviceImpl/DepartmentServiceImp.java
+++ employee_manager/src/main/java/com/example/employee_manager/service/serviceImpl/DepartmentServiceImp.java
@@ -1,28 +1,33 @@
 package com.example.employee_manager.service.serviceImpl;
 
 import com.example.employee_manager.domain.Department;
-import com.example.employee_manager.domain.Position;
+import com.example.employee_manager.domain.Employee;
 import com.example.employee_manager.repository.DepartmentRepository;
+import com.example.employee_manager.repository.EmployeeRepository;
 import com.example.employee_manager.service.DepartmentService;
 import com.example.employee_manager.service.dto.DepartmentDTO;
-import com.example.employee_manager.service.dto.PositionDTO;
 import com.example.employee_manager.service.mapper.DepartmentMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @Transactional
 public class DepartmentServiceImp implements DepartmentService {
-
     private final DepartmentMapper departmentMapper;
     private final DepartmentRepository departmentRepository;
+    private final EmployeeRepository employeeRepository;
 
-    public DepartmentServiceImp(DepartmentMapper departmentMapper, DepartmentRepository departmentRepository) {
+    public DepartmentServiceImp(DepartmentMapper departmentMapper, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
         this.departmentMapper = departmentMapper;
         this.departmentRepository = departmentRepository;
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
@@ -37,8 +42,17 @@ public class DepartmentServiceImp implements DepartmentService {
 
     @Override
     public DepartmentDTO save(DepartmentDTO departmentDTO) {
-
         Department department = departmentMapper.toEntity(departmentDTO);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            Optional<Employee> optionalEmployee = employeeRepository.findOneByUserName(userDetails.getUsername());
+            if (optionalEmployee.isPresent()) {
+                Employee user = optionalEmployee.get();
+                department.setCreatedBy(user.getUserName());
+            }
+        }
+        department.setCreatedDate(new Date());
         department = departmentRepository.save(department);
         return departmentMapper.toDto(department);
     }
@@ -46,6 +60,15 @@ public class DepartmentServiceImp implements DepartmentService {
     @Override
     public DepartmentDTO update(DepartmentDTO departmentDTO) {
         Department department =departmentMapper.toEntity(departmentDTO);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            Optional<Employee> optionalEmployee = employeeRepository.findOneByUserName(userDetails.getUsername());
+            if (optionalEmployee.isPresent()) {
+                Employee user = optionalEmployee.get();
+                department.setLastModifiedBy(user.getUserName());
+            }
+        }
         department.setId(department.getId());
         departmentRepository.save(department);
         DepartmentDTO departmentDTO1 = departmentMapper.toDto(department);
